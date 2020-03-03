a3de79c82f52544a7c1bf5e2b591c118f91b990b
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-03 15:58:37 +0100

    Add & implement SocialContainer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <branding-container />
     <status-container class="statusContainer" />
+    <social-container class="socialContainer" />
   </div>
 </template>
 
 <script>
 import BrandingContainer from './components/BrandingContainer.vue';
 import StatusContainer from './components/StatusContainer.vue';
+import SocialContainer from './components/SocialContainer.vue';
 
 export default {
   components: {
     BrandingContainer,
     StatusContainer,
+    SocialContainer,
   },
 };
 </script>
@@ -27,7 +30,8 @@ export default {
 </style>
 
 <style lang="scss" scoped>
-.statusContainer {
+.statusContainer,
+.socialContainer {
   margin-top: 60px;
 }
 </style>
---
+++ src/components/SocialContainer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="socialContainer">
+    <a href="https://twitter.com/TribecampStatus" target="_blank" class="btnTwitter">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <a href="https://instagram.com/tribecampofficial" target="_blank" class="btnInstagram">
+      <i class="fab fa-instagram"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.socialContainer {
+  text-align: center;
+
+  a {
+    margin-right: 10px;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+
+  i {
+    font-size: 24px;
+
+    &.fa-twitter {
+      color: #1DA1F2;
+    }
+
+    &.fa-instagram {
+      color: #e35ef5;
+    }
+  }
+}
+</style>
