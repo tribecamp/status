ceff75eba12be2a2c53b66a65d70d5f01bba3bc6
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-11-11 14:58:08 +0100

    chore(deps): use new ESLint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   extends: [
-    './node_modules/@tribecamp/eslint/base',
-    './node_modules/@tribecamp/eslint/vue'
+    '@tribecamp/base',
+    '@tribecamp/vue'
   ]
 };
